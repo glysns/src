70adcfd6eab53f346c12afe34469d9cef80146e6
Gleyson Sampaio <izapay4devs@gmail.com>
2024-02-25 09:35:17 -0300

    navegation

---
+++ nuxt-starterkit/4-nuxt-starter-content/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Teste
+        <ContentNavigation v-slot="{ navigation}">
+            <ul>
+                <li v-for="link of navigation" :key="link._path">
+                    <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+                </li>
+            </ul>
+        </ContentNavigation>
+        <NuxtPage/>
+    </div>
+</template>
